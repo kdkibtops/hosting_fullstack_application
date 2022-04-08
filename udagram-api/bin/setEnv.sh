8c80bf005409047ce9ab9f2d4fc2fbf605fee3ce
kdkibtops <kdkibtops@gmail.com>
2022-04-08 08:48:23 +0200

    changed JSON scripts

--- udagram-api/bin/setEnv.sh
+++ udagram-api/bin/setEnv.sh
@@ -1,4 +1,4 @@
-eb init udagramheidar --platform node.js --region us-east-1
+eb init udagramheidar 
 eb use Udagramheidar-env
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
