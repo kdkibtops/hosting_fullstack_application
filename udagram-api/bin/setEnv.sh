dc5a09f3439cdb2b0f7d2a5ef5bd316537feede9
kdkibtops <kdkibtops@gmail.com>
2022-04-08 08:54:22 +0200

    new update

--- udagram-api/bin/setEnv.sh
+++ udagram-api/bin/setEnv.sh
@@ -1,4 +1,4 @@
-eb init udagramheidar 
+eb init udagramheidar -p node.js --region us-east-1f
 eb use Udagramheidar-env
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
